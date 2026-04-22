8ab505c746ff0a9c435f480af957e2be8828f8ed
wcheaz <winstoncheaz@gmail.com>
2026-04-22 13:27:05 -0400

    Arhived proposal for the image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -226,7 +226,7 @@ function CustomInput(props: InputProps) {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-[#007fd4]">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
               </svg>
-              <span className="text-sm text-[#d4d4d4] font-medium truncate max-w-[150px]">{file.name}</span>
+              <span className="text-sm text-[#d4d4d4] font-medium truncate max-w-37.5">{file.name}</span>
               <button
                 onClick={() => removeFile(index)}
                 className="ml-1 text-[#858585] hover:text-[#d4d4d4] focus:outline-none"
@@ -407,7 +407,9 @@ function YourMainContent({
       style={{}}
       className="h-screen flex items-center pt-[10vh] flex-col transition-colors duration-300"
     >
-      <AddDesignButton state={state} setState={setState} />
+      {process.env.NODE_ENV === "development" && (
+        <AddDesignButton state={state} setState={setState} />
+      )}
       <DesignComponent state={state} setState={setState} />
     </div>
   );
